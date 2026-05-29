b5b18f3457d876cdc1429a5acc5d37b1220ccb8f
PAX <shreyworkshappily@gmail.com>
2026-05-29 15:00:23 +0530

    --

--- conditions/conditional_expressions.py
+++ conditions/conditional_expressions.py
@@ -8,4 +8,10 @@ num = 7
 
 logged_in = False
 
+age = 18
+temp = 40
+
 print("Welcome!" if logged_in else "Please log in to continue.")
+
+print("you are an adult" if age>=20 else "You need to wait until you are 20 to be an adult")
+print("It is hot outside" if temp > 35 else "The weather is nice outside")
